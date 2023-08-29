2855171e87264f3d9b162673b295b9fc111aefe4
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-29 22:00:43 +0600

    Marvel Characters - API, Spinner / Loading Func

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,38 @@
+import { Component } from "react/cjs/react.production.min";
+
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
-import decoration from '../../resources/img/vision.png';
+import decoration from "../../resources/img/vision.png";
 
-const App = () => {
+class App extends Component {
+  onCharSelected = (id) => {
+    this.setState({
+      selectedChar: id,
+    });
+  };
+
+  state = {
+    selectedChar: null,
+  };
+
+  render() {
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+      <div className="app">
+        <AppHeader />
+        <main>
+          <RandomChar />
+          <div className="char__content">
+            <CharList onCharSelected={this.onCharSelected} />
+            <CharInfo charId={this.state.selectedChar} />
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
