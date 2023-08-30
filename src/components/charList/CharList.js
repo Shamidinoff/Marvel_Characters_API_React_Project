2a52ee3956d93c0f6ef8410fd4cae178c0f39f23
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-30 11:10:12 +0600

    Marvel Characters - API, Spinner / Loading Func

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -10,6 +10,7 @@ class CharList extends Component {
     loading: true,
     error: false,
     newItemLoading: false,
+    offset: 210,
   };
 
   marvelService = new MarvelService();
@@ -19,17 +20,26 @@ class CharList extends Component {
   }
 
   onRequest = (offset) => {
+    this.onCharListLoading();
     this.marvelService
       .getAllCharacters(offset)
       .then(this.onCharListLoaded)
       .catch(this.onError);
   };
 
-  onCharListLoaded = (charList) => {
+  onCharListLoading = () => {
     this.setState({
-      charList,
-      loading: false,
+      newItemLoading: true,
     });
+  };
+
+  onCharListLoaded = (newCharList) => {
+    this.setState(({ offset, charList }) => ({
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+    }));
   };
 
   onError = () => {
@@ -67,7 +77,7 @@ class CharList extends Component {
   }
 
   render() {
-    const { charList, loading, error } = this.state;
+    const { charList, loading, error, offset, newItemLoading } = this.state;
 
     const items = this.renderItems(charList);
 
@@ -80,7 +90,11 @@ class CharList extends Component {
         {errorMessage}
         {spinner}
         {content}
-        <button className="button button__main button__long">
+        <button
+          className="button button__main button__long"
+          disabled={newItemLoading}
+          onClick={() => this.onRequest(offset)}
+        >
           <div className="inner">load more</div>
         </button>
       </div>
