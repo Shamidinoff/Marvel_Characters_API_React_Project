a851d273f12f21c1219c08f9c7db07948f2b2aeb
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-30 11:52:39 +0600

    Marvel Characters - API, Spinner / Loading Func

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -11,6 +11,7 @@ class CharList extends Component {
     error: false,
     newItemLoading: false,
     offset: 210,
+    charEnded: false,
   };
 
   marvelService = new MarvelService();
@@ -34,11 +35,17 @@ class CharList extends Component {
   };
 
   onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+
     this.setState(({ offset, charList }) => ({
       charList: [...charList, ...newCharList],
       loading: false,
       newItemLoading: false,
       offset: offset + 9,
+      charEnded: ended,
     }));
   };
 
@@ -77,7 +84,8 @@ class CharList extends Component {
   }
 
   render() {
-    const { charList, loading, error, offset, newItemLoading } = this.state;
+    const { charList, loading, error, offset, newItemLoading, charEnded } =
+      this.state;
 
     const items = this.renderItems(charList);
 
@@ -93,6 +101,7 @@ class CharList extends Component {
         <button
           className="button button__main button__long"
           disabled={newItemLoading}
+          style={{ display: charEnded ? "none" : "block" }}
           onClick={() => this.onRequest(offset)}
         >
           <div className="inner">load more</div>
