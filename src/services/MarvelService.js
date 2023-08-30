54607b128093a33db1adba42f76a69d0d504081f
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-30 10:44:19 +0600

    Marvel Characters - API, Spinner / Loading Func

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -15,9 +15,9 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = async () => {
+  getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=${this._baseOffset}&${this._apiKey}`
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
     );
     return res.data.results.map(this._transformCharacter);
   };
